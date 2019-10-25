af4c5646ace2aa28cbc7d9e13082cce081f0e9fc
diahanggraini13 <diahanggraini13@gmail.com>
2019-10-25 21:52:43 +0700

    Upload Tugas Studi Kasus 01

---
+++ 2120180144-K01-PBO-TI20192020-3-4-01/src/pkg2120180144/k01/pbo/ti20192020/pkg3/pkg4/pkg01/K01PBOTI201920203401.java
@@ -0,0 +1,36 @@
+package pkg2120180144.k01.pbo.ti20192020.pkg3.pkg4.pkg01;
+public class K01PBOTI201920203401 {
+    public static void main(String[] args) {
+
+        //instansiasi dari class mahasiswa
+    MAHASISWA mahasiswa_1 = new MAHASISWA();
+    mahasiswa_1.alamat="Desa Sumbang Timun RT/RW 15/05 Trucuk Bojonegoro";
+    mahasiswa_1.nama="Diah Anggraini";
+    mahasiswa_1.nim="2120180144";
+    mahasiswa_1.biodata();//akses method menampilkan Biodata Diah Anggraini
+    
+    MAHASISWA mahasiswa_2 = new MAHASISWA();
+    mahasiswa_2.alamat="Jl. Brigjen Sutoya No 16 Sukorejo Bojonegoro";
+    mahasiswa_2.nama="Erlinda Nofa Prastianti";
+    mahasiswa_2.nim="2120180149";
+    mahasiswa_2.biodata();//Menampilkan Biodata Erlinda Nofa Prastianti
+            
+    MAHASISWA mahasiswa_3 = new MAHASISWA();
+    mahasiswa_3.alamat="Jl. Letda Mustajab No 14A Sukorejo Bojonegoro";
+    mahasiswa_3.nama="Arini Mailiyal Haq";
+    mahasiswa_3.nim="2120180137";
+    mahasiswa_3.biodata();//Menampilkan Biodata Arini Mailiyal Haq
+    
+    MAHASISWA mahasiswa_4 = new MAHASISWA();
+    mahasiswa_4.alamat="Desa Sumberarum RT/RW 17/06 Dander Bojonegoro";
+    mahasiswa_4.nama="Galih Parwati Septiani";
+    mahasiswa_4.nim="2120180151";
+    mahasiswa_4.biodata();//Menampilkan Biodata Galih Parwati Septiani
+    
+    MAHASISWA mahasiswa_5 = new MAHASISWA();
+    mahasiswa_5.alamat="Desa Kunci RT/RW 23/01 Dander Bojonegoro";
+    mahasiswa_5.nama="Indah Purnama Sari";
+    mahasiswa_5.nim="2120180158";
+    mahasiswa_5.biodata();//menampilkan Biodata Indah Purnama Sari
+    }
+  }
---
+++ 2120180144-K01-PBO-TI20192020-3-4-01/src/pkg2120180144/k01/pbo/ti20192020/pkg3/pkg4/pkg01/MAHASISWA.java
@@ -0,0 +1,15 @@
+package pkg2120180144.k01.pbo.ti20192020.pkg3.pkg4.pkg01;
+public class MAHASISWA {
+    //properti yang digunakan
+    public String alamat;
+    public String nama;
+    public String nim;
+    
+    //fungsi : method
+    public void biodata(){
+    System.out.println("Alamat :" + alamat);
+    System.out.println("Nama :" + nama);
+    System.out.println("Nim : " + nim);
+    }
+    
+}
